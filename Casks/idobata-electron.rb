ceb708545bb05f9e61239b1a69c19bff1956418a
Hiroshi Yoshida <hrysd22@gmail.com>
2016-01-28 21:03:25 +0900

    Update idobata-electron

--- Casks/idobata-electron.rb
+++ Casks/idobata-electron.rb
@@ -1,6 +1,6 @@
 cask 'idobata-electron' do
-  version '0.4.1'
-  sha256 '7b553a73a63723205610841643db93a6404756b8f4ad7e6687a2db6c7be69435'
+  version '0.4.2'
+  sha256 '7d074f329b53e1d18e5737d8337ffb32f1133871a9f7d807098a3f919212b7a5'
 
   url "https://github.com/hrysd/idobata-electron/releases/download/v#{version}/idobata-electron-v#{version}-darwin.zip"
   appcast 'https://github.com/hrysd/idobata-electron/releases.atom'
